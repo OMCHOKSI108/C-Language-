f957816a42a7cff975f8391c9cf92a19696c86f5
OMCHOKSI108 <134709654+OMCHOKSI108@users.noreply.github.com>
2023-06-08 16:12:07 +0530

    Add files via upload

---
+++ OM-4222/day11/task5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+	int n;
+	int i=1;
+	
+	printf("Enter Value : ");
+	scanf("%d",&n);
+	
+	while(i<=n){
+		while(i%2==1){
+		
+	
+		
+		printf("%d\n",i);
+		i++;		
+}
+
+     
+		i++;		
+	} 
+	
+return 0;
+}
