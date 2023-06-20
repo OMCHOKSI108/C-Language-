e6cd3fb2ea451fa49268bf28d44216bf1cb37d0d
OMCHOKSI108 <134709654+OMCHOKSI108@users.noreply.github.com>
2023-06-20 07:59:08 -0500

    Add files via upload

---
+++ OM-4222/day18/task9.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+int main(){
+	int n,i,m,j;
+	
+	printf("Enter Number of Rows    :");
+	scanf("%d",&n);
+	
+	printf("Enter Number of Columns :");
+	scanf("%d",&m);
+	printf("\n");
+	int a[n][m];
+	for(i=0;i<n;i++){
+		for(j=0;j<m;j++){
+			printf("Enter Element a[%d][%d] :",i,j);
+			scanf("%d",&a[i][j]);
+		}
+		
+	}
+	printf("\n");
+	for(i=0;i<n;i++){
+		for(j=0;j<m;j++){
+			printf("%d  ",a[i][j]);
+		
+		}
+		printf("\n");
+	}
+		printf("\n");
+		int sum=0;
+	for(i=0;i<n;i++){
+		for(j=m;j>=0;j--){
+			if(i==j){
+				
+				sum+=a[i][j];
+			}
+		
+		}
+		printf("\n");
+	}
+		for(i=0;i<n;i++){
+		for(j=0;j<m;j++){
+			if(i==j){
+				
+				sum+=a[i][j];
+			}
+		
+		}
+		printf("\n");
+	}
+	printf("Cross-diagonal Sum Is : %d",sum);
+	
+	
+	return 0;
+}
+
