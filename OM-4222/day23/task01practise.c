af4fd9bd07aa70a4b945e3184e642f7d3000d6a6
OMCHOKSI108 <134709654+OMCHOKSI108@users.noreply.github.com>
2023-06-27 18:52:53 +0530

    Add files via upload

---
+++ OM-4222/day23/task01practise.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+
+void sum(int a,int b){
+	
+	printf("Sum : %d\n\n",a+b);
+	
+}
+
+void sub(int a,int b){
+	
+	printf("Substraction : %d\n\n",a-b);
+	
+}
+void multi(int a,int b){
+	
+	printf("Multiplication : %d\n\n",a*b);
+	
+}
+void div(int a,int b){
+	
+	printf("Division : %d\n\n",a/b);
+	
+}
+
+int main(){
+		
+	int a,b;
+	char sign;
+  q:
+	
+	printf("Enter First Value  : ");
+	scanf("%d",&a);
+	
+	printf("Enter Second Value : ");
+	scanf("%d",&b);
+	
+	printf("\nMake Selection\n + , - , x , / :");
+	scanf(" %c",&sign);
+	
+	switch(sign){
+		
+		case '+':
+		sum(a,b);
+		
+			break ;
+		case '-':
+			
+		sub(a,b);
+			break ;
+		case 'x':
+		
+		multi(a,b);
+			break ;
+		
+		case '/':
+		
+		div(a,b);
+			break ;
+		
+		
+		
+	}
+	goto q;
+	return 0;
+}
+
