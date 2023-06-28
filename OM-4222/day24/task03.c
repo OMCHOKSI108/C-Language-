ce58475af602b65c6b835e0d1bfad7ef310169e3
OMCHOKSI108 <134709654+OMCHOKSI108@users.noreply.github.com>
2023-06-28 21:34:46 +0530

    Add files via upload

---
+++ OM-4222/day24/task03.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+ void length(char  arr[100]){
+  	int p=0,c=0;
+  
+
+while(arr[c]!='\0'){
+	
+	
+	c++;
+}
+
+
+	  printf("Length Of Array : %d",c);
+	
+}
+
+
+int main(){
+  int i;
+  char arr[100];
+        printf("Enter Element :");
+       gets(arr);
+length(arr);
+	return 0;
+}
